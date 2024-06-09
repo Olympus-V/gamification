b71a709d25be2c09e2908c69d1f7f95eda9c79f9
raghu0812199 <1941012115.b.balaboinaraghuyadav@gmail.com>
2024-06-10 00:41:51 +0530

    Feat-Memory Lane Setup

--- src/pages/Home/home.tsx
+++ src/pages/Home/home.tsx
@@ -1,27 +1,29 @@
-import { useNavigate } from 'react-router-dom';
-import './home.scss'
+import { useNavigate } from "react-router-dom";
+import "./home.scss";
 
 const HomePage = () => {
-    const navigate = useNavigate();
-    return (
-
-        <div className="buttons-container">
-            <button
-                onClick={() => { navigate('/SpinTheWheel') }}
-               >
-                Spin The Wheel
-            </button>
-            <button>Hurdle Cuddle</button>
-            <button>Know Me Better</button>
-            <button
-                onClick={() => { navigate('/SpankBank') }}>
-                Spank Bank
-            </button>
-            <button>Memory Lane</button>
-
-        </div>
-
-    )
-}
+  const navigate = useNavigate();
+  return (
+    <div className="buttons-container">
+      <button
+        onClick={() => {
+          navigate("/SpinTheWheel");
+        }}
+      >
+        Spin The Wheel
+      </button>
+      <button>Hurdle Cuddle</button>
+      <button>Know Me Better</button>
+      <button
+        onClick={() => {
+          navigate("/SpankBank");
+        }}
+      >
+        Spank Bank
+      </button>
+      <button onClick={() => navigate("/MemoryLane")}>Memory Lane</button>
+    </div>
+  );
+};
 
 export default HomePage;
