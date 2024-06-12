82e38f00f09e220f1e115ce18363da373da13c1d
Deb Adarsh Sahu <deb.sahu@maersk.com>
2024-06-13 00:54:23 +0530

    Fix- Updated public asset paths for env build

--- src/pages/Games/games.tsx
+++ src/pages/Games/games.tsx
@@ -7,27 +7,27 @@ const GamesPage = () => {
         <div className='games-page d-flex flex-col justify-content-center'>
             <div className='games-content d-flex flex-row justify-content-center'>
                 <div className='game-card'>
-                    <img src='/images/SpinTheWheel/game_icon.svg' alt='Spin The Wheel' />
+                    <img src={`${process.env.PUBLIC_URL}/images/SpinTheWheel/game_icon.svg`} alt='Spin The Wheel' />
                     <button onClick={() => { navigate('/games/spinTheWheel') }}>Spin The Wheel</button>
                 </div>
                 <div className='game-card'>
-                    <img src='/images/HurdleCuddle/game_icon.svg' alt='Hurdle Cuddle' />
+                    <img src={`${process.env.PUBLIC_URL}/images/HurdleCuddle/game_icon.svg`} alt='Hurdle Cuddle' />
                     <button>Hurdle Cuddle</button>
                 </div>
                 <div className='game-card'>
-                    <img src='/images/KnowMeBetter/game_icon.svg' alt='Know Me Better' />
+                    <img src={`${process.env.PUBLIC_URL}/images/KnowMeBetter/game_icon.svg`} alt='Know Me Better' />
                     <button>Know Me Better</button>
                 </div>
                 <div className='game-card'>
-                    <img src='/images/KnowMeBetterAdult/game_icon.svg' alt='Know Me Better Adult' />
+                    <img src={`${process.env.PUBLIC_URL}/images/KnowMeBetterAdult/game_icon.svg`} alt='Know Me Better (Adult)' />
                     <button>Know Me Better (Adult)</button>
                 </div>
                 <div className='game-card'>
-                    <img src='/images/SpankBank/game_icon.svg' alt='Spank Bank' />
+                    <img src={`${process.env.PUBLIC_URL}/images/SpankBank/game_icon.svg`} alt='Spank Bank' />
                     <button onClick={() => { navigate('/games/spankBank') }}>Spank Bank</button>
                 </div>
                 <div className='game-card'>
-                    <img src='/images/MemoryLane/game_icon.svg' alt='Memory Lane' />
+                    <img src={`${process.env.PUBLIC_URL}/images/MemoryLane/game_icon.svg`} alt='Memory Lane' />
                     <button onClick={() => navigate('/games/memoryLane')}>Memory Lane</button>
                 </div>
             </div>
