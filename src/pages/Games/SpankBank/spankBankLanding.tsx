0db930fd66a390f779f57618e9b104bc9014eec0
Satyajit Panigrahi <satyapani999@gmail.com>
2024-07-20 23:05:51 +0530

    added bg dropdown

--- src/pages/Games/SpankBank/spankBankLanding.tsx
+++ src/pages/Games/SpankBank/spankBankLanding.tsx
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
-import './spankBankLanding.scss';
-import ImageUpload from '../../../components/ImageUpload/imageUpload';
-import SpankBank from './spankBank';
+import React, { useState } from "react";
+import "./spankBankLanding.scss";
+import ImageUpload from "../../../components/ImageUpload/imageUpload";
+import SpankBank from "./spankBank";
 
 const SpankBankLandingPage: React.FC = () => {
-  const [isImageValid, setIsImageValid] = useState<boolean>(false);
+  const [isImageValid, setIsImageValid] = useState<boolean>(true);
   const [isFormSubmitted, setIsFormSubmitted] = useState<boolean>(false);
 
   const handleImageValidation = (isValid: boolean) => {
@@ -19,29 +19,38 @@ const SpankBankLandingPage: React.FC = () => {
   return (
     <div className="input-details">
       {!isFormSubmitted ? (
-        !isImageValid ? (
+        <form onSubmit={handleSubmit} className="custom-form">
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input type="text" id="name" name="name" required />
+          </div>
+          <div className="form-group">
+            <label htmlFor="audio">Upload Audio:</label>
+            <input
+              type="file"
+              id="audio"
+              name="audio"
+              accept="audio/*"
+              required
+            />
+          </div>
           <ImageUpload onImageValidate={handleImageValidation} />
-        ) : (
-            <form onSubmit={handleSubmit} className="custom-form">
-            <div className="form-group">
-              <label htmlFor="name">Name:</label>
-              <input type="text" id="name" name="name" required />
-            </div>
-            <div className="form-group">
-              <label htmlFor="audio">Upload Audio:</label>
-              <input type="file" id="audio" name="audio" accept="audio/*" required />
-            </div>
-            <div className="form-group">
-              <label htmlFor="image">Upload Image:</label>
-              <input type="file" id="image" name="image" accept="image/*" required />
-            </div>
-            <div className="form-group">
-              <label htmlFor="message">Custom Message:</label>
-              <textarea id="message" name="message" required />
-            </div>
-            <button type="submit" className="submit-button">SPANK!</button>
-          </form>
-        )
+          <div className="form-group">
+            <label htmlFor="dropdown">Choose an Option:</label>
+            <select id="dropdown" name="dropdown">
+              <option value="option1">Bedroom</option>
+              <option value="option2">Classroom 2</option>
+              <option value="option3">Bathroom</option>
+            </select>
+          </div>
+          <div className="form-group">
+            <label htmlFor="message">Custom Message:</label>
+            <textarea id="message" name="message" required />
+          </div>
+          <button type="submit" className="submit-button">
+            SPANK!
+          </button>
+        </form>
       ) : (
         <SpankBank />
       )}
